5e0b229fc3a49f661e27f131b23d6266bafda97b
Aluckard <aluckard8888@gmail.com>
2020-01-11 18:49:33 +0300

    готовый 4 урок

--- learn4/js.js
+++ learn4/js.js
@@ -7,57 +7,85 @@ let money = 50000,
     mission = 250000,
     period = 6;
 
-console.log(typeof (money));
-console.log(typeof (income));
-console.log(typeof (deposit));
-console.log(income.length);
-console.log("за " + period + " месяцев " + mission + " рублей на отпуск");
-console.log(addExpenses.toLowerCase().split(", "));
+let showTypeOf = function(data) {
+    console.log(data, typeof(data));
+};
+
+showTypeOf(money);
+showTypeOf(income);
+showTypeOf(deposit);
+
+
+showTypeOf(income.length);
+showTypeOf("за " + period + " месяцев " + mission + " рублей на отпуск");
+showTypeOf(addExpenses.toLowerCase().split(", "));
 
 let days = 30;
 
 let budgetDay = function (money, days) {
     return (money / days) + " " + (money % days);
-};
-console.log(budgetDay(money, days));
+}
+showTypeOf(budgetDay(money, days));
 
 
-money = +prompt("Ваш месячный доход?");
+money = +prompt("Ваш месячный доход?", "80000");
 
 addExpenses = prompt ("Перечислите возможные расходы за расчитываемый период через запятую!", "Интернет, телефон, бензин");
-console.log(addExpenses.split(", "));
+showTypeOf(addExpenses.split(", "));
 
 deposit = confirm("Есть ли у вас депозит в банке?");
 
-console.log(typeof (money));
-console.log(typeof (income));
-console.log(typeof (deposit));
+showTypeOf(typeof (money));
+showTypeOf(typeof (income));
+showTypeOf(typeof (deposit));
 
-let costs = prompt("Какие обязательные ежемесячные расходы у вас есть?");
-let howMuch = +prompt("Во сколько это обойдёться?");
-let costs2 = prompt("Какие обязательные ежемесячные расходы у вас есть?");
-let howMuch2 = +prompt("Во сколько это обойдёться?");
+let costs = prompt("Какие обязательные ежемесячные расходы у вас есть?", "бензин");
+let howMuch = +prompt("Во сколько это обойдёться?", "3000");
+let costs2 = prompt("Какие обязательные ежемесячные расходы у вас есть?", "интерет");
+let howMuch2 = +prompt("Во сколько это обойдёться?", "1000");
 
 function sum(howMuch, howMuch2, money) {
     return money-(howMuch + howMuch2);
 }
 let budgetMonth = sum(howMuch, howMuch2, money);
-console.log(budgetMonth);
+showTypeOf(budgetMonth);
 
-function sum2(mission, budgetMonth) {
+function missionAccomplishment(mission, budgetMonth) {
     return mission / budgetMonth;
 }
-period = sum2(mission, budgetMonth);
-console.log(Math.ceil(period));
+
+let getMissionAccomplishment = missionAccomplishment(mission, budgetMonth);
+showTypeOf(Math.ceil(getMissionAccomplishment));
 
 function sum3(budgetMonth, days) {
     return budgetMonth / days;
-};
+}
 budgetDay = sum3(budgetMonth, days);
 
-console.log(Math.floor(budgetDay));
+showTypeOf(Math.floor(budgetDay));
 
-if (budgetDay >= 800){console.log("Высокий уровень дохода!");}
-else if (budgetDay >= 300 && budgetDay < 800){console.log("Средний уровень дохода!");}
-else if (budgetDay >= 0 && budgetDay < 300){console.log("Низкий уровень дохода!");}
-else  {console.log("Что то пошло не так!");}
+let getStatusIncome = function () {
+    if (budgetDay >= 800){return ("Высокий уровень дохода!");}
+    else if (budgetDay >= 300 && budgetDay < 800){return ("Средний уровень дохода!");}
+    else if (budgetDay >= 0 && budgetDay < 300){return ("Низкий уровень дохода!");}
+    else  {return ("Что то пошло не так!");}
+};
+
+showTypeOf(getStatusIncome());
+
+function getExpensesMonth(howMuch, howMuch2) {
+return howMuch + howMuch2;
+}
+showTypeOf(getExpensesMonth(howMuch, howMuch2));
+
+let accumulatedMonth = budgetMonth;
+showTypeOf(accumulatedMonth);
+
+let getTargetMonth = missionAccomplishment(mission, budgetMonth);
+showTypeOf(Math.floor(getTargetMonth) + " месяца для достижения цели!");
+
+function accumulatedPeriod (period, accumulatedMonth) {
+return period * accumulatedMonth;
+}
+
+showTypeOf(Math.ceil(accumulatedPeriod(period, accumulatedMonth)) + " накопишь за " + Math.floor(period) + " месяцев");
